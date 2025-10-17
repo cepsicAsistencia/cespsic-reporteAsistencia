759799aeae773928d10a24139e1af8f167a5df3a
cepsicAsistencia <cepsic.atencionpsicologica@gmail.com>
2025-10-16 21:00:30 -0700

    Update reports.js

--- js/reports.js
+++ js/reports.js
@@ -15,8 +15,8 @@ const GOOGLE_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbyN49EgjqFoE4
 //const SHEET_ID = '1YLmEuA-O3Vc1fWRQ1nC_BojOUSVmzBb8QxCCsb5tQwk';
 //const GOOGLE_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbzBJRaLjii8Y8F_9XC3_n5e--R2bzDXqrfWHeFUIYn3cRct-qVHZ1VEgJEj8XKEU9Ch/exec';
 
-//'jose.lino.flores.madrigal@gmail.com',
 const ADMIN_USERS = [
+    'jose.lino.flores.madrigal@gmail.com',
     'cepsic.atencionpsicologica@gmail.com',
     'cespsic@uas.edu.mx',
     'adymadrid.22@gmail.com'
