91cf951cb0487166985c5db89c9146b62a418ec9
cepsicAsistencia <cepsic.atencionpsicologica@gmail.com>
2025-11-10 09:00:54 -0700

    Update Config.js

--- js/Config.js
+++ js/Config.js
@@ -63,9 +63,9 @@ const AMBIENTES = {
 const AUTH_CONFIG = {
   // Google Client ID para Google Sign-In
   //PRODUCCION
-  //GOOGLE_CLIENT_ID: '799841037062-kal4vump3frc2f8d33bnp4clc9amdnng.apps.googleusercontent.com',
-	//PRUEBAS
-	GOOGLE_CLIENT_ID: '154864030871-ck4l5krb7qm68kmp6a7rcq7h072ldm6g.apps.googleusercontent.com',
+  GOOGLE_CLIENT_ID: '799841037062-kal4vump3frc2f8d33bnp4clc9amdnng.apps.googleusercontent.com',
+  //PRUEBAS
+  //GOOGLE_CLIENT_ID: '154864030871-ck4l5krb7qm68kmp6a7rcq7h072ldm6g.apps.googleusercontent.com',
   
   // Usuarios administradores con acceso completo
   ADMIN_USERS: [
